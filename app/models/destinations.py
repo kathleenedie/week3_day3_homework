b484a31d96024dfb47dd440cc7abd6bb5b773605
KathleenEdie <kathleenedie@gmail.com>
2021-01-20 20:42:05 +0000

    css working for main page but not second html file

--- app/models/destinations.py
+++ app/models/destinations.py
@@ -4,4 +4,5 @@ from app.models.order import *
 order1 = Order("John R. Edie", "20-01-20", 2, "Penang")
 order2 = Order("Bel Baxter", "15-01-21", 3, "Santiago")
 order3 = Order("Michael Ferguson", "18/10/21", 9, "Entebbe")
-orders = [order1, order2, order3]
+order4 = Order("Kirsten D'Arcy", "25/04/21", 3, "Entebbe")
+orders = [order1, order2, order3, order4]
